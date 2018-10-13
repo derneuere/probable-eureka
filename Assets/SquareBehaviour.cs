24cb6fd3cf704bb297c31f2a1245f6708d5fee64
Moritz Voss <mail@moritzvoss.com>
2018-10-13 14:27:19 +0200

    Simple collisions

---
+++ Assets/SquareBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SquareBehaviour : MonoBehaviour
+{
+    public bool Dead;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!Dead)
+        {
+            Dead = true;
+            GetComponentInChildren<SpriteRenderer>().sprite = null;
+            Destroy(other.gameObject);
+        }
+    }
+}
