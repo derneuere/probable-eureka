cd59333506c98bc2ca2470e25a3d5bde9d924924
Moritz Voss <mail@moritzvoss.com>
2018-10-13 20:11:54 +0200

    Game starts

---
+++ Assets/StartWhenReady.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartWhenReady : MonoBehaviour
+{
+    public PlayerControls pc;
+    public VolcanoBehaviour volcano;
+    
+    // Update is called once per frame
+    private void Update()
+    {
+        if (pc._unassignedPlayers.Count == 0)
+        {
+            volcano.enabled = true;
+            Destroy(this);
+        }
+    }
+}
