00974852bded1dc7f430a7d29c2cf715c9d20565
Moritz Voss <mail@moritzvoss.com>
2018-10-13 18:42:57 +0200

    Bubble stuff

--- Assets/Scripts/LavaBlobBehaviour.cs
+++ Assets/Scripts/LavaBlobBehaviour.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class LavaBlobBehaviour : MonoBehaviour
 {
+    public GameObject[] BubblePrefabs;
+
+    public float BubbleMinTime = 0.3f;
+    public float BubbleMaxTime = 0.7f;
+
+    private float _time;
+    
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         GetComponent<Animator>().speed = Random.Range(0.5f, 2.0f);
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        _time -= Time.deltaTime;
+
+        if (_time < 0)
+        {
+            _time = Random.Range(BubbleMinTime, BubbleMaxTime);
+
+            SpawnBubble(Random.insideUnitCircle * 0.2f);
+        }
+    }
+
+    private void SpawnBubble(Vector2 push)
+    {
+        var pos = Random.insideUnitCircle * 0.5f;
+        pos.y = Mathf.Abs(pos.y) + 0.1f;
+        pos = pos + (Vector2) transform.position;
         
+        var go =  Instantiate(BubblePrefabs.Pick(), pos, Quaternion.identity);
+        go.GetComponent<Rigidbody2D>().AddRelativeForce(push, ForceMode2D.Impulse);
+    }
+
+    private void OnDestroy()
+    {
+        for (var i = 0; i < 20; i++)
+        {
+            SpawnBubble(Random.insideUnitCircle);
+        }
     }
 }
