389bdccdfa4f55c1824e59f21b61d13e26b319c9
Thygrrr <github@thygrrr.de>
2018-10-20 17:49:36 +0200

    Music experiments

--- Assets/Voss/Util/Audio/MinMaxRangeAttribute.cs
+++ Assets/Voss/Util/Audio/MinMaxRangeAttribute.cs
@@ -2,11 +2,11 @@
 
 public class MinMaxRangeAttribute : Attribute
 {
-	public MinMaxRangeAttribute(float min, float max)
-	{
-		Min = min;
-		Max = max;
-	}
-	public float Min { get; private set; }
-	public float Max { get; private set; }
+    public MinMaxRangeAttribute(float min, float max)
+    {
+        Min = min;
+        Max = max;
+    }
+    public float Min { get; private set; }
+    public float Max { get; private set; }
 }
