de28389a48b5a12f3f15ffce168c654235838069
Klay <klayton.carlos1995@gmail.com>
2018-10-27 13:52:04 +0200

    UI prefab

---
+++ Assets/Scripts/DegreeUI.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class DegreeUI : MonoBehaviour
+{
+    public TMP_Text text;
+
+    public void SetDegree(int degree)
+    {
+        text.SetText("degree: " + degree);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
