6183cd2b7107ae6b69242171c54f07dfd014fade
Leon <leonobendorf@yahoo.com>
2018-10-27 16:18:04 +0200

    LeonsszeneLebensverlustBeiGeschenksexplosion

--- Assets/Scripts/scrGeschenk.cs
+++ Assets/Scripts/scrGeschenk.cs
@@ -8,6 +8,7 @@ public class scrGeschenk : MonoBehaviour
     public AudioEvent destroysound;
     public GameObject view;
     public GameObject magicpoofGeschenk;
+    public GameManager lebensAnderung;
     // Start is called before the first frame update
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -18,9 +19,10 @@ public class scrGeschenk : MonoBehaviour
             destroysound.Play(GetComponent<AudioSource>());
             GameObject temp = Instantiate(magicpoofGeschenk,transform,false);
             temp.gameObject.transform.localScale = new Vector3(5,5,5);
-
+            lebensAnderung.livePoints = lebensAnderung.livePoints - 1;
             Destroy(view);
             Destroy(gameObject,3.0f);
+
             Destroy(this);
             ScreenShakeProfile.trauma = 1.0f;
 
