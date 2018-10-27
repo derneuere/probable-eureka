3f7515479c0be010bd0d04f38adc7ab50dac1b34
Moritz Voss <mail@moritzvoss.com>
2018-10-27 14:58:16 +0200

    refactor

--- Assets/Follow.cs
+++ Assets/Follow.cs
@@ -5,12 +5,12 @@ public class Follow : MonoBehaviour
     public Transform Target;
     public float Smoothing = 0.5f;
     
-    private Vector3 velocity;
+    private Vector3 _velocity;
 
     private void Update()
     {
         var goal = Target.position;
         goal.z = transform.position.z;
-        transform.position = Vector3.SmoothDamp(transform.position, goal, ref velocity, Smoothing);
+        transform.position = Vector3.SmoothDamp(transform.position, goal, ref _velocity, Smoothing);
     }
 }
