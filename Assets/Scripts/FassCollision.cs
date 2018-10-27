c13088bb76f0c534a0ebd2262df10744c9038a3c
Klay <klayton.carlos1995@gmail.com>
2018-10-27 15:00:31 +0200

    fass collision

---
+++ Assets/Scripts/FassCollision.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FassCollision : MonoBehaviour
+{
+
+    public AudioEvent destroysound;
+    // Start is called before the first frame update
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Players"))
+        {
+
+            destroysound.Play(GetComponent<AudioSource>());
+            Destroy(gameObject, 1.0f);
+            Destroy(this);
+
+
+
+        }
+    }
+}
