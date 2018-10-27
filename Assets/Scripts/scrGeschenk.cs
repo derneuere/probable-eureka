457c71542ff13b0d670d99dc5951e64a6e4fa2dd
Leon <leonobendorf@yahoo.com>
2018-10-27 16:47:00 +0200

    Tempchange

--- Assets/Scripts/scrGeschenk.cs
+++ Assets/Scripts/scrGeschenk.cs
@@ -8,7 +8,7 @@ public class scrGeschenk : MonoBehaviour
     public AudioEvent destroysound;
     public GameObject view;
     public GameObject magicpoofGeschenk;
-    public GameManager lebensAnderung;
+    public GameManager temperaturAnderung;
     // Start is called before the first frame update
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -17,13 +17,19 @@ public class scrGeschenk : MonoBehaviour
 
 
             destroysound.Play(GetComponent<AudioSource>());
+
             GameObject temp = Instantiate(magicpoofGeschenk,transform,false);
+
             temp.gameObject.transform.localScale = new Vector3(5,5,5);
-            lebensAnderung.startDegree = lebensAnderung.startDegree - 2;
+
+            temperaturAnderung.startDegree = temperaturAnderung.startDegree - 2;
+
             Destroy(view);
+
             Destroy(gameObject,3.0f);
 
             Destroy(this);
+
             ScreenShakeProfile.trauma = 1.0f;
 
 
